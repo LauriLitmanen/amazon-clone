89c1b49683428a58c8d126a96759a23b91627630
LauriLitmanen <litmanen.lauri@gmail.com>
2020-09-11 14:58:03 +0300

    [First commit]Header component

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBqSyVcwFuVgynk4RoP56dQVhJoxC1SVPc",
+    authDomain: "clone-c97cd.firebaseapp.com",
+    databaseURL: "https://clone-c97cd.firebaseio.com",
+    projectId: "clone-c97cd",
+    storageBucket: "clone-c97cd.appspot.com",
+    messagingSenderId: "993895341387",
+    appId: "1:993895341387:web:cac39920d6d09f56fa1cce",
+    measurementId: "G-TF3K62VPSB"
+  };
