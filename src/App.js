058985851057f11da3536917364f06298f035497
LauriLitmanen <litmanen.lauri@gmail.com>
2020-09-12 12:20:24 +0300

    [Product, Checkout, Subtotal, datalayer, React router] Added orduct checkout and subtotal components and react router, configured the data layer for handling adding and removing items from the basket

--- src/App.js
+++ src/App.js
@@ -3,15 +3,32 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import Checkout from './Checkout';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     //BEM
-    <div className="App">
+    <Router>
+      <div className="App">
       <Header></Header>
-      <Home></Home>
-      { /**Home */ }
-    </div>
+        <Switch>
+        
+          <Route path="/checkout">
+           
+            <Checkout></Checkout>
+          </Route>
+          <Route path="/">
+            
+            <Home></Home>
+          </Route>
+          
+          
+        </Switch>
+        
+        { /**Home */ }
+      </div>
+    </Router>
     
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,67 @@
 import React from 'react'
 import "./Home.css";
+import Product from './Product';
 
 function Home() {
     return (
         <div className="home">
-            <p>home</p>
+           <div className="home__container">
+               <img 
+                className="home__image"
+                src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" 
+                alt="banner"
+                />
+                <div className="home__row">
+                  <Product
+                  id="0123"
+                  title="The Lean startup"
+                  price={29.99}
+                  image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
+                  rating={5}
+                  />
+                   <Product
+                   id="09123"
+                  title="The Lean startup"
+                  price={29.99}
+                  image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
+                  rating={5}
+                  />
+                  
+                </div>
+                <div className="home__row">
+                <Product
+                    id="123"
+                  title="The Lean startup"
+                  price={29.99}
+                  image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
+                  rating={5}
+                  />
+                 <Product
+                 id="1234"
+                  title="The Lean startup"
+                  price={29.99}
+                  image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
+                  rating={5}
+                  />
+                 <Product
+                 id="12345"
+                  title="The Lean startup"
+                  price={29.99}
+                  image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
+                  rating={5}
+                  />
+                </div>
+                <div className="home__row">
+                <Product
+                id="123456"
+                  title="The Lean startup"
+                  price={29.99}
+                  image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
+                  rating={5}
+                  />
+                    
+                </div>
+           </div>
         </div>
     )
 }
